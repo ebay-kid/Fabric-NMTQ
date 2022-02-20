b5352897ea925e8fd48800ccc531636d5c32b969
AC <addison@abcis.me>
2022-02-19 21:10:49 -0800

    first commit

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,16 +1,31 @@
 package net.fabricmc.example.mixin;
 
 import net.fabricmc.example.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.util.math.Vec3d;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(TitleScreen.class)
+import java.util.Collections;
+import java.util.Set;
+
+@Mixin(ServerPlayNetworkHandler.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
-	}
+    @Redirect(
+            method = "onPlayerMove",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/math/Vec3d;lengthSquared()D",
+                    ordinal = 0
+            )
+    )
+    public double preLengthSquared(Vec3d instance){
+        return 1000000000;
+    }
 }
